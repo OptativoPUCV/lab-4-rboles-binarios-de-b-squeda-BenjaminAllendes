607236c84710d510075424b2eadb00cbd2247946
Your Name <you@example.com>
2025-04-29 20:30:48 -0400

    new try

--- treemap.c
+++ treemap.c
@@ -73,7 +73,7 @@ void eraseTreeMap(TreeMap * tree, void* key){
 Pair * searchTreeMap(TreeMap * tree, void* key) {
 
     tree->current = tree->root ;
-    while (tree->current->left == NULL && tree->current->right == NULL)
+    while (tree->current->left != NULL && tree->current->right != NULL)
     {
         int resultado = tree->lower_than(tree->current->pair->key, key) ;
         if (resultado == 1) {
@@ -85,7 +85,6 @@ Pair * searchTreeMap(TreeMap * tree, void* key) {
             tree->current  = tree->current->left ;
         }
     }
-    printf("Hola") ;
     return NULL;
 }
 
