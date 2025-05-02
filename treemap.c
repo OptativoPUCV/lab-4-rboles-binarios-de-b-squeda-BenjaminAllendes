ccbe1c37c2f9fa0a53eb4df52ba2ce5d5f87b8e4
Your Name <you@example.com>
2025-05-02 00:48:40 -0400

    new try

--- treemap.c
+++ treemap.c
@@ -181,6 +181,7 @@ Pair * upperBound(TreeMap * tree, void* key) {
         }
         if (tree->current != NULL && tree->lower_than(node->pair->key, tree->current->pair->key) && tree->lower_than(tree->current->pair->key, key)) {
             node = tree->current ;
+            printf("Se cumple =ded") ;
         }
     }
     
